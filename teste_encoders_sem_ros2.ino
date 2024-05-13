029ae2bb9f6ac15dbaf396414f3ec9b8c007ac1b
Matheus Rodrigues <162641773+RodriguesMat@users.noreply.github.com>
2024-05-13 14:59:00 -0300

    Update teste_encoders_sem_ros2.ino
    Json

--- teste_encoders_sem_ros2.ino
+++ teste_encoders_sem_ros2.ino
@@ -1,5 +1,6 @@
 #include <Sabertooth.h>
-//#include <Encoder.h>
+#include <Encoder.h>
+#include <ArduinoJson.h>
 
 #include <stdio.h>
 
@@ -16,9 +17,9 @@ long odom_timer;
 
 float enc1, enc2, enc3;
 
-//Encoder Encoder1(2, 3);
-//Encoder Encoder2(4, 5);
-//Encoder Encoder3(20, 21);
+Encoder Encoder1(33, 35);
+Encoder Encoder2(37, 39);
+Encoder Encoder3(41, 43);
 
 float map(float x, float
 in_min, float in_max, float out_min, float out_max) {
@@ -26,7 +27,7 @@ in_min, float in_max, float out_min, float out_max) {
 }
 
 int convertToMotor(float value) {
-  float r = map(value, -1.0, 1.0, -127.0, 127.0);
+  float r = map(value, -10.0, 10.0, -127.0, 127.0);
   r = constrain(r, -127.0, 127.0);
   return (int) round(r);
 }
@@ -44,9 +45,9 @@ void stopMotors() {
 }
 
 void clearEncoders() {
-  //Encoder1.write(0);
-  //Encoder2.write(0);
-  //Encoder3.write(0);
+  Encoder1.write(0);
+  Encoder2.write(0);
+  Encoder3.write(0);
 }
 
 void setup() {
@@ -54,108 +55,45 @@ void setup() {
   pinMode(12, INPUT_PULLUP);
   pinMode(13, INPUT_PULLUP);
   
-
   SabertoothTXPinSerial.begin(9600);
 
   Serial.begin(9600);
 
   stopMotors();
 
-  // clearEncoders();
+  clearEncoders();
 
-  // odom_timer = millis();
-
+  odom_timer = millis();
 
 }
+
 void loop() {
-  Serial.println("Teste");
-  for (float i=0;i<0.2;i+=0.02){
-    moveMotors(convertToMotor(i/2), convertToMotor(i/2), convertToMotor(-i));
-  }
-  delay(3000);
-
-  moveMotors(convertToMotor(0.0), convertToMotor(0.0), convertToMotor(0.0));
-  delay(500);
-
-  for (float i=0;i<0.2;i+=0.01){
-    moveMotors(convertToMotor(-i), convertToMotor(i), convertToMotor(0.0));
-  }
-  delay(3000);
-
-  moveMotors(convertToMotor(0.0), convertToMotor(0.0), convertToMotor(0.0));
-  delay(500);
-
-  for (float i=0;i<0.2;i+=0.01){
-    moveMotors(convertToMotor(-i/2), convertToMotor(-i/2), convertToMotor(i));
-  }
-
-  delay(3000);
-
-  moveMotors(convertToMotor(0.0), convertToMotor(0.0), convertToMotor(0.0));
-  delay(500);
-
-  for (float i=0;i<0.2;i+=0.01){
-    moveMotors(convertToMotor(i), convertToMotor(-i), convertToMotor(0.0));
-  }
-
-  delay(3000);
-
-  moveMotors(convertToMotor(0.0), convertToMotor(0.0), convertToMotor(0.0));
-  delay(500);
-}
-// //movimento 1
   
-//   moveMotors(convertToMotor(0.0), convertToMotor(0.2), convertToMotor(-0.2));
-//   delay(2000);
-   
-  
-// //pausa  
-//   moveMotors(convertToMotor(0.0), convertToMotor(0.0), convertToMotor(0.0));
-//   delay(700);
+  if (millis() - odom_timer >= ODOM_PERIOD) {
 
-// //movimento 2
-//   moveMotors(convertToMotor(0.3), convertToMotor(-0.2), convertToMotor(-0.2));
-//   delay(2000);
+    enc1 = Encoder1.read();
+    enc2 = Encoder2.read();
+    enc3 = Encoder3.read();
 
-// //pausa  
-//   moveMotors(convertToMotor(0.0), convertToMotor(0.0), convertToMotor(0.0));
-//   delay(700);
+    // Serial.println(enc1 + enc2 + enc3);
 
-
-// //movimento 3
-
-//   moveMotors(convertToMotor(0.0), convertToMotor(-0.2), convertToMotor(0.2));
-//   delay(2000);
-
-// //pausa  
-//   moveMotors(convertToMotor(0.0), convertToMotor(0.0), convertToMotor(0.0));
-//   delay(700);
-
-
-// //movimento 4  
-//   moveMotors(convertToMotor(-0.3), convertToMotor(0.2), convertToMotor(0.2));
-//   delay(2000);
-
-
-// //pausa  
-//   moveMotors(convertToMotor(0.0), convertToMotor(0.0), convertToMotor(0.0));
-//   delay(700);
-
-
-// // if (millis() - odom_timer >= ODOM_PERIOD) {
-
-//     // enc1 = Encoder1.read();
-//     // enc2 = Encoder2.read();
-//     // enc3 = Encoder3.read();
-
-//     // Serial.println(enc1);
-//     // Serial.println(enc2);
-//     // Serial.println(enc3);
-//     // Serial.println("-----");
+    StaticJsonDocument<512> doc;
     
-    // odom_timer = millis();
+    String cmd = Serial.readString();
+    deserializeJson(doc, cmd);
+    double m1 = doc["motors"][3];
+    double m2 = doc["motors"][2];
+    double m3 = doc["motors"][1];
 
-    // clearEncoders();
+    moveMotors(convertToMotor(m1), convertToMotor(m2), convertToMotor(m3));
+
+   
+    
+    odom_timer = millis();
+
+    clearEncoders();
 
 
-  //}
+  }
+
+}
